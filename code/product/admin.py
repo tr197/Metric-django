d20263bc748f3b1fc9e87407a95c8a6cf2c5084d
tr197 <huutri26697@gmail.com>
2023-11-18 11:25:14 +0700

    admin product page

--- code/product/admin.py
+++ code/product/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from product.models import Category, CategorySub, Product, ProductImages
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+
+
+
+class ProductImageInLine(admin.TabularInline):
+    model = ProductImages
+    extra = 0
+
+
+class ProductAdmin(admin.ModelAdmin):
+    exclude = ('slug',)
+    inlines = [ProductImageInLine, ]
+    ordering= ['category', 'slug',]
+    list_filter = ['category',]
+    search_fields = ['slug']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(CategorySub, CategoryAdmin)
+admin.site.register(Product, ProductAdmin)
